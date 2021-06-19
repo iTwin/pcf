af6cb7b9da01c1b8edef742fc7be88af856fbdfa
zachzhu2016 <zachzhu2016@gmail.com>
2021-06-19 18:58:25 -0400

    wip

--- core/src/fwk/Runner.ts
+++ core/src/fwk/Runner.ts
@@ -1,93 +1,136 @@
 import { ChangesType } from "@bentley/imodelhub-client";
 
-export async function runConnector() {}
-
-export interface IBuilder {
-  db: IModelDb;
-
-  onChangeChannel(newParentId: Id64String): void;
-  enterChannel(channelRootId: Id64String, lockRoot?: boolean): void;
-
-  init(): Promise<void>;
-  acquireDb(): Promise<void>;
-  updateData(): Promise<void>;
-  updateSchemas(): Promise<void>;
-  updateDefinitions(): Promise<void>;
-  finalizeChanges(): Promise<void>;
+export interface IDbController {
+  commit(desc: string, ctype: ChangesType): Promise<void>;
 }
 
-export interface BriefcaseDbBuilderProps {
+export interface DbControllerProps {
+  connectorModulePath: string;
+  sourcePath: boolean;
   jobSubjectName: string;
+  updateDbProfile?: boolean;
+  updateDomainSchemas?: boolean;
+}
+
+export interface SnapshotDbControllerProps extends DbControllerProps {
+  db: SnapshotDb;
+}
+
+export interface StandaloneDbControllerProps extends DbControllerProps {
+  db: StandaloneDb;
+}
+
+export interface BriefcaseDbControllerProps extends DbControllerProps {
+  db: BriefcaseDb;
   reqContext: AuthorizedClientRequestContext;
   iModelId: Id64String;
   contextId: Id64String;
-  updateDbProfile: boolean;
-  updateDomainSchemas: boolean;
 }
 
-class BriefcaseDbBuilder implements IBuilder {
+export async function runConnector(db: IModelDb, args: any) {
+
+  let controller;
+  if (db instanceof BriefcaseDb)
+    controller = new BriefcaseDbController();
+  else if (db instanceof SnapshotDb)
+    controller = new SnapshotDbController();
+  else if (db instanceof StandaloneDb)
+    controller = new StandaloneDbController();
+  else
+    throw new Error("db must be one of them - BriefcaseDb, SnapshotDb, or StandaloneDb");
+
+  const db = await controller.acquire();
+  const connector = loadConnector();
+
+  // TODO Refactor Synchronizer 
+  const synchronizer = new Synchronizer(db, connector.supportsMultipleFilesPerChannel(), reqContext);
+  connector.synchronizer = synchronizer;
+
+  const { jobSubjectName } = args;
+  let jobSub = findJobSubject(db, jobSubjectName);
+  const isBriefcaseDb = db instanceof BriefcaseDb;
+
+  if (isBriefcaseDb)
+    db.concurrencyControl.startBulkMode();
+
+  if (!jobSub) {
+    if (isBriefcaseDb)
+      await this.enterSharedChannel();
+    
+    jobSub = insertJobSubject(db, connector, args.sourcePath);
+    await controller.persistChanges("Inserted Connector Job Subject", ChangesType.GlobalProperties);
+
+    if (isBriefcaseDb)
+      await this.enterNormalChannel();
+
+    await connector.initializeJob();
+    await controller.persistChanges("Initialized Connector Job", ChangesType.Regular);
+  }
+
+  connector.jobSubject = jobSub;
+
+  if (isBriefcaseDb)
+    await this.enterNormalChannel();
+
+  await connector.importDefinitions(reqContext);
+  await controller.persistChanges("Definition Changes", ChangesType.Definition);
+
+  if (isBriefcaseDb)
+    await this.enterSharedChannel();
+
+  await connector.importDomainSchema(reqContext);
+  await controller.persistChanges("Domain Schema Changes", ChangesType.Schema);
+
+  if (isBriefcaseDb)
+    await this.enterSharedChannel();
+
+  await connector.importDynamicSchema(reqContext);
+  await controller.persistChanges("Dynamic Schema Changes", ChangesType.Schema);
+
+  if (isBriefcaseDb)
+    await this.enterNormalChannel();
+
+  await connector.updateExistingData();
+  await controller.persistChanges(this._connector.getDataChangesDescription() ?? "Data Changes", ChangesType.Regular);
+
+  if (isBriefcaseDb)
+    await this.enterNormalChannel();
+
+  if (args.doDetectDeletedElements) {
+    synchronizer.detectDeletedElements();
+    updateProjectExtents(db);
+    await controller.persistChanges("Finalization Changes", ChangesType.Regular);
+  }
+}
+
+export class BriefcaseDbControllerProps implements IDbController {
 
   private _db?: BriefcaseDb;
-  private _connector: Connector;
   private _activityId: GuidString;
-  private _props: BriefcaseDbBuilderProps;
+  private _props: BriefcaseDbControllerProps;
 
-  constructor(connector: ITwinConnector, props: BriefcaseDbBuilderProps) {
-    this._connector = connector;
+  constructor(db: BriefcaseDb, props: BriefcaseDbControllerProps) {
+    this._db = db;
     this._activityId = Guid.createValue();
     this._props = props;
   }
 
-  public async dispatchUpdate(ctype: ChangesType, desc: string, func: async () => Promise<any>) {
-    switch(ctype) {
-      case ChangesType.Schema:
-      case ChangesType.GlobalProperties:
-        await this.enterRepositoryChannel();
-        if (!this._db.concurrencyControl.locks.hasSchemaLock);
-          throw new Error("does not have schema lock");
-        if (!briefcaseDb.concurrencyControl.isBulkMode);
-          throw new Error("not in bulk mode");
-        break;
-      case ChangesType.Definition:
-        await this.enterConnectorChannel();
-        if (this._db.concurrencyControl.locks.hasSchemaLock);
-          throw new Error("has schema lock");
-        if (!briefcaseDb.concurrencyControl.isBulkMode);
-          throw new Error("not in bulk mode");
-        break;
-
-      case ChangesType.Regular:
-      case ChangesType.SheetsAndDrawings:
-      case ChangesType.SpatialData:
-      case ChangesType.ViewsAndModels:
-        if (this._db.concurrencyControl.locks.hasSchemaLock)
-          throw new Error("has schema lock");
-        if (!this._db.concurrencyControl.isBulkMode)
-          throw new Error("not in bulk mode");
-        await this.enterConnectorChannel();
-        if (!this._db.concurrencyControl.channel.isChannelRootLocked)
-          throw new Error("channel root not locked");
-        break;
-    }
-    await func();
-
+  public async commit(desc: string, ctype: ChangesType) {
     await this._db.concurrencyControl.request(reqContext);
     await this._db.pullAndMergeChanges(reqContext);
-    this._db.saveChanges();
 
-    const comment = this.createComment(desc);
+    const { revisionComments } = this._props;
+    const comment = this.createComment(revisionComments, desc);
+    this._db.saveChanges(comment);
     await this._db.pushChanges(reqContext, comment, ctype);
   }
 
-  public async init() {
-    const { reqContext, contextId } = this._props;
+  public async acquire(): BriefcaseDb {
+    const { contextId, iModelId, reqContext, updateDbProfile, updateDomainSchemas } = this._props;
     UsageLoggingUtilities.postUserUsage(reqContext, contextId, IModelJsNative.AuthType.OIDC, os.hostname(), IModelJsNative.UsageType.Trial).catch((err) => {
       Logger.logError(ConnectorLoggerCategory.Framework, `Could not log user usage for connector`, () => ({ errorStatus: err.status, errorMessage: err.message }));
     });
-  }
 
-  public async acquireDb(): Promise<void> {
-    const { contextId, iModelId, reqContext, updateDbProfile, updateDomainSchemas } = this._props;
     const briefcaseId = this._connectorArgs.argsJson ? this._connectorArgs.argsJson.briefcaseId : undefined;
     const req = briefcaseId ? { contextId, iModelId, briefcaseId } : { contextId, iModelId };
     const bcProps: LocalBriefcaseProps = await BriefcaseManager.downloadBriefcase(reqContext, req);
@@ -97,130 +140,70 @@ class BriefcaseDbBuilder implements IBuilder {
 
     const openArgs: OpenBriefcaseProps = { fileName: bcProps.fileName };
     this._db = await BriefcaseDb.open(reqContext, openArgs);
-
-    const synchronizer = new Synchronizer(briefcaseDb, this._connector.supportsMultipleFilesPerChannel(), reqContext);
-    this._connector.synchronizer = synchronizer;
-
     this._db.concurrencyControl.startBulkMode();
+
+    return this._db;
   }
+}
 
-  protected createComment(changeDesc: string): string {
-    const { revisionComments } = this._props;
-    const title = revisionComments.substring(0. 400);
-    const comment = title.length > 0 ? `${title} - ${changeDesc}` : changeDesc;
-    return comment;
+async function commit(db: IModelDb, reqContext: AuthorizedClientRequestContext, comment: string, ctype: ChangesType) {
+  if (db instanceof BriefcaseDb) {
+    await db.concurrencyControl.request(reqContext);
+    await db.pullAndMergeChanges(reqContext);
+    db.saveChanges(comment);
+    await db.pushChanges(reqContext, comment, ctype);
+  } else if (db instanceof SnapshotDb || db instanceof StandalongDb) {
+    db.saveChanges(comment);
   }
+}
 
-  protected updateJobSubject() {
-    await this.enterConnectorChannel();
-    const { jobSubjectName } = this._props;
-    const existingSubject = findJobSubject(this._db, jobSubjectName);
+async function enterSharedChannel(db: BriefcaseDb, reqContext: AuthorizedClientRequestContext) {
+  await enterChannel(db, IModelDb.repositoryModelId, reqContext);
+  if (!db.concurrencyControl.locks.hasSchemaLock);
+    throw new Error("does not have schema lock");
+}
 
-    if (existingSubject)
-      this._connector.jobSubject = existingSubject;
-    else
-      const newSubj = this.insertJobSubject();
-  }
+async function enterNormalChannel(db: BriefcaseDb, subId: Id64String, reqContext: AuthorizedClientRequestContext) {
+  await enterChannel(db, subId, reqContext);
+  if (db.concurrencyControl.locks.hasSchemaLock);
+    throw new Error("has schema lock");
+}
 
-  protected importDefinitions() {
-    await this.enterConnectorChannel();
-    const { jobSubjectName } = this._props;
-    const existingSubj = findJobSubject(this._db, jobSubjectName);
+async function enterChannel(db: BriefcaseDb, rootId: Id64String, reqContext: AuthorizedClientRequestContext) {
+  if (db.concurrencyControl.hasPendingRequests)
+    throw new Error("has pending requests");
+  if (!db.concurrencyControl.isBulkMode)
+    throw new Error("not in bulk mode");
+  if (db.concurrencyControl.locks.hasSchemaLock)
+    throw new Error("has schema lock");
+  if (db.concurrencyControl.locks.hasCodeSpecsLock)
+    throw new Error("has code spec lock");
+  if (db.concurrencyControl.channel.isChannelRootLocked())
+    throw new Error("holds lock on current channel root. it must be released before entering a new channel.");
+  db.concurrencyControl.channel.channelRoot = rootId;
+  await db.concurrencyControl.channel.lockChannelRoot(reqContext);
+  if (!db.concurrencyControl.channel.isChannelRootLocked)
+    throw new Error("channel root not locked");
+}
 
-    if (existingSubj) {
-      this._connector.jobSubject = existingSubj;
-    } else {
-      const newSubj = this.insertJobSubject();
-      await this.saveAndPushChanges("Inserted Connector Job Subject", ChangesType.GlobalProperties);
+function createComment(revisionComments: string, changeDesc: string): string {
+  const title = revisionComments.substring(0. 400);
+  const comment = title.length > 0 ? `${title} - ${changeDesc}` : changeDesc;
+  return comment;
+}
 
-      await this.enterConnectorChannel();
-      this._connector.jobSubject = newSubj;
-      await this._connector.initializeJob();
-      await this.saveAndPushChanges("Initialized Connector Job Subject", ChangesType.Regular);
-      await this.enterRepositoryChannel();
-    }
+function loadConnector(connectorModulePath: string): Connector {
+  const connectorModule = require(connectorModulePath);
+  return connectorModule.getConnectorInstance();
+}
 
-    await this._connector.importDefinitions();
-    await this.saveAndPushChanges("Definition changes", ChangesType.Definition);
-  }
-
-  protected updateSchemas() {
-    const { reqContext } = this._props;
-
-    if (this._db.concurrencyControl.locks.hasSchemaLock)
-      throw new Error("has schema lock");
-    if (!this._db.concurrencyControl.isBulkMode)
-      throw new Error("not in bulk mode");
-
-    await this.saveAndPushChanges("Initialization", ChangesType.Definition);
-
-    await this.enterRepositoryChannel();
-    await this._connector.importDomainSchema(reqContext);
-    await this.saveAndPushChanges("Schema changes", ChangesType.Schema);
-
-    await this.enterRepositoryChannel();
-    await this._connector.importDynamicSchema(reqContext);
-    await this._db.concurrencyControl.request(reqContext);
-    return this.saveAndPushChanges("Dynamic schema changes", ChangesType.Schema);
-  }
-
-  protected updateExistingData() {
-    const { reqContext } = this._props;
-
-    if (this._db.concurrencyControl.locks.hasSchemaLock)
-      throw new Error("has schema lock");
-    if (!this._db.concurrencyControl.isBulkMode)
-      throw new Error("not in bulk mode");
-
-    await this.enterConnectorChannel();
-    if (!this._imodel.concurrencyControl.channel.isChannelRootLocked)
-      throw new Error("channel root not locked");
-
-    await this._connector.updateExistingData();
-
-    let changesDesc = "Data changes";
-    if (this._connector.getDataChangesDescription)
-      changesDesc = this._connector.getDataChangesDescription();
-
-    await this.saveAndPushChanges(changesDesc, ChangesType.Regular);
-  }
-
-  public async enterRepositoryChannel() {
-    const lockRoot = true;
-    this.enterChannel(IModelDb.repositoryModelId, lockRoot);
-  }
-
-  public async enterConnectorChannel() {
-    if (!this._jobSubject)
-      throw new Error("job subject undefined");
-    return this._enterChannel(this._jobSubject.id, lockRoot);
-  }
-
-  protected onChangeChannel(newParentId: Id64String) {
-    if (this._db.concurrencyControl.hasPendingRequests)
-      throw new Error("has pending requests");
-    if (!this._db.concurrencyControl.isBulkMode)
-      throw new Error("not in bulk mode");
-    if (this._db.concurrencyControl.locks.hasSchemaLock)
-      throw new Error("has schema lock");
-    if (this._db.concurrencyControl.locks.hasCodeSpecsLock)
-      throw new Error("has code spec lock");
-
-    const root = this._db.concurrencyControl.channel.channelRoot;
-    if (root) {
-      const lockOnRoot = ConcurrencyControl.Request.getElementLock(root, LockScope.Exclusive);
-      const holdsRootLock = this._db.concurrencyControl.locks.holdsLock(lockOnRoot);
-      if (holdsRootLock)
-        throw new Error("holds lock on channel root");
-    }
-  }
-
-  protected enterChannel(channelRootId: Id64String, lockRoot: boolean = true) {
-    const { reqContext } = this._props;
-    this._db.concurrencyControl.channel.channelRoot = channelRootId;
-    if (lockRoot)
-      this._db.concurrencyControl.channel.lockChannelRoot(reqContext);
-  }
+function updateProjectExtents(db: IModelDb) {
+  const options: ComputeProjectExtentsOptions = {
+    reportExtentsWithOutliers: false,
+    reportOutliers: false,
+  };
+  const res = db.computeProjectExtents(options);
+  db.updateProjectExtents(res.extents);
 }
 
 function insertJobSubject(db: IModelDb, connector: Connector, subjName: string): Subject {
@@ -260,19 +243,5 @@ function findJobSubject(db: IModelDb, subjName: string): Subject | undefined {
   if (subjId)
     return db.elements.tryGetElement<Subject>(subjId);
   return undefined;
-}
-
-function loadConnector(connectorModulePath: string): Connector {
-  const connectorModule = require(connectorModulePath);
-  return connectorModule.getConnectorInstance();
-}
-
-function updateProjectExtents(db: IModelDb) {
-  const options: ComputeProjectExtentsOptions = {
-    reportExtentsWithOutliers: false,
-    reportOutliers: false,
-  };
-  const res = db.computeProjectExtents(options);
-  db.updateProjectExtents(res.extents);
 }
 
