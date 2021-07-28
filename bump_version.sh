a9c6ef1acdcad9ccf8df754c3a1f42738a788a70
Zachary Zhu <Zachary.Zhu@bentley.com>
2021-07-28 17:32:15 -0400

    Integrates release pipeline

---
+++ bump_version.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd core
+npm version patch
+cd ..
+
+cd cli
+npm version patch
+cd ..
+
+git add ./**/package.json ./**/package-lock.json
+
+version=$(node -p "require('./core/package.json').version")
+git commit -m "$version"
+
